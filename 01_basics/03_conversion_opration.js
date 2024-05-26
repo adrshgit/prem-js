9af049576d0c16a4784b96336f462ad3da8ccc85
AD₹SH <140497925+adrshgit@users.noreply.github.com>
2024-05-26 19:15:02 +0000

    prefix and postfix increment

--- 01_basics/03_conversion_opration.js
+++ 01_basics/03_conversion_opration.js
@@ -66,3 +66,23 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// postfix increment
+
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+
+
+// prefix increment
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
